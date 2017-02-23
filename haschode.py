ddd10ce70e53107716b73235dc5dffc3a0b7e8f5
XaviMorenoM <xavier.moreno.martinez@gmail.com>
2017-02-23 19:53:21 +0100

    Populating object info

--- haschode.py
+++ haschode.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from CacheServer import CacheServer
+from Endpoint import Endpoint
 
 def main():
     pass
@@ -9,28 +10,33 @@ def main():
 
 def read_file(file_name):
     with open(file_name, 'r') as f:
-
+        l_caches = []
         videos, endpoints, request_descriptions, caches, cache_size = f.readline().split(' ')
+
+        for i in xrange(0, int(caches)):
+            l_caches.append(CacheServer(i, int(cache_size)))
+
         video_file_sizes = [int(x) for x in f.readline().split(' ')]
-        endpoints = [[] for _ in range(int(endpoints))]
+        endpoints = [Endpoint(_) for _ in xrange(int(endpoints))]
+
         #parse endpoint cfg
-        for idx in range(len(endpoints)):
+        for idx in xrange(len(endpoints)):
             v = f.readline().split(' ')
             ms = int(v[0])
             nu = int(v[1])
-            endpoints[idx] = [ms, nu, []]
-            for _ in range(nu):
-                endpoints[idx][2].append(int(f.readline().split(' ')[1]))
+            endpoints[idx].add_latency(ms)
+
+            for cache_index in xrange(nu):
+                endpoints[idx].add_latency(int(f.readline().split(' ')[1]), cache_index)
 
         #parse video requests
-        request_descriptions = [[] for _ in range(int(request_descriptions))]
-        for idx in range(len(request_descriptions)):
-            request_descriptions[idx] = [int(x) for x in f.readline().split(' ')]
+        for idx in xrange(int(request_descriptions)):
+            video_id, endpoint_id, num_requests = (int(x) for x in f.readline().split(' '))
+            endpoints[endpoint_id].add_request(video_id, num_requests)
 
 
-        print videos, caches, caches
-        print endpoints
-        print request_descriptions
+
+        return l_caches, endpoints
 
 
 
